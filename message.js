20d49d961604a7bd6d348c4439cdc0d410559610
BIKRAM BHAGAT <bikrambhagat55@gmail.com>
2022-10-30 21:53:55 +0530

    file is ready

--- message.js
+++
@@ -1,9 +0,0 @@
-import { name, age } from "./person.js";
-
-const message = () => {
-    const name = "Jesse";
-    const age = 40;
-    return name + ' is ' + age + 'years old.';
-    };
-    
-    export default message;
--- person.js
+++
@@ -1,5 +0,0 @@
-import message from "./message.js";
-const name = "Jesse";
-const age = 40;
-
-export {name, age};
